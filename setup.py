dbc56d25726a63fa663f335b777fb0d35a4b5cfa
daniel garcia vaglio <degv364@gmail.com>
2018-02-16 08:25:32 -0600

    Make setup.py PEP8 compliant

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 
-setup(name='vfl',
-      version='0.1',
-      description='Vector Field Library',
-      author='Federico Ruiz Ugalde',
-      author_email='memeruiz@gmail.com',
-      url='http://www.arcoslab.org/',
-      packages=['vfl'],
-     )
+setup(
+    name='vfl',
+    version='0.1',
+    description='Vector Field Library',
+    author='Federico Ruiz Ugalde',
+    author_email='memeruiz@gmail.com',
+    url='http://www.arcoslab.org/',
+    packages=['vfl'],
+)
